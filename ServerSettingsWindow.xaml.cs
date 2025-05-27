47dd41f5e00bb9e2b9193bd4eed6510c7f824b28
Doomkid <adautovnneto@gmail.com>
2025-05-27 20:20:15 -0300

    Update ServerSettingsWindow.xaml.cs

--- ServerSettingsWindow.xaml.cs
+++ ServerSettingsWindow.xaml.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IniParser.Model;
+using IniParser;
+using IniParser.Parser;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace DDO_Launcher
 {
@@ -38,5 +38,7 @@ namespace DDO_Launcher
         {
             this.Close();
         }
+
+
     }
 }
