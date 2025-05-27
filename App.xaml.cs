d226f860072e164dafaf01bdca4da7dcb5b205c3
Doomkid <adautovnneto@gmail.com>
2025-05-27 20:15:31 -0300

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,11 @@ namespace DDO_Launcher;
 /// </summary>
 public partial class App : Application
 {
-
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        var serverManager = new ServerManager("DDO_Launcher.ini");
+        var mainWindow = new MainWindow(serverManager);
+        mainWindow.Show();
+    }
 }
 
