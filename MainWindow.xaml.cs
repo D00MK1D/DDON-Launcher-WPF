d94fe1543e1dae5ed1046feea998422bf147d2f1
Doomkid <144261348+D00MK1D@users.noreply.github.com>
2025-10-21 06:18:33 -0300

    prepare for old translation implementation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
-using System.Security.Policy;
 using System.Text;
 using System.Text.Json;
 using System.Windows;
