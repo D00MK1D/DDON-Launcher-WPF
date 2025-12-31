d40ca0f524ab9394e106faa4af3fc2826dccad0d
Doomkid <144261348+D00MK1D@users.noreply.github.com>
2025-12-31 08:53:48 -0300

    Admin rights require message

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Security.Principal;
 using System.Windows;
 
 namespace DDO_Launcher;
@@ -11,9 +12,28 @@ public partial class App : Application
 {
     private void Application_Startup(object sender, StartupEventArgs e)
     {
+        if (!IsRunAsAdmin())
+        {
+            MessageBox.Show(
+                "Please, run the launcher as administrator.",
+                "Attention!",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+            );
+
+            Environment.Exit(0);
+        }
+
         var serverManager = new ServerManager("DDO_Launcher.ini");
         var mainWindow = new MainWindow(serverManager);
         mainWindow.Show();
+    }
+
+    private static bool IsRunAsAdmin()
+    {
+        using var identity = WindowsIdentity.GetCurrent();
+        var principal = new WindowsPrincipal(identity);
+        return principal.IsInRole(WindowsBuiltInRole.Administrator);
     }
 }
 
