4491d4b165c725377d4ecd8f9336dff253a91b7d
Doomkid <144261348+D00MK1D@users.noreply.github.com>
2026-02-24 00:35:04 -0300

    Revert "Correction of translation repo default english link"
    This reverts commit b5f665010e51f2daf9f4f836c1c04893b1acb328.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -49,12 +49,6 @@ public partial class MainWindow : Window, INotifyPropertyChanged
             cbRemember.IsChecked = Properties.Settings.Default.rememberMe;
             serverComboBox.SelectedIndex = Properties.Settings.Default.lastServerSelected;
         }
-
-        if (Properties.Settings.Default.translationPatchUrl.EndsWith("heads/main/gmd.csv"))
-        {
-            Properties.Settings.Default.translationPatchUrl = "https://raw.githubusercontent.com/Sapphiratelaemara/DDON-translation/refs/heads/main/English/gmd.csv";
-            Properties.Settings.Default.Save();
-        }
     }
 
     public class ServerResponse
@@ -418,7 +412,7 @@ public partial class MainWindow : Window, INotifyPropertyChanged
             if (Properties.Settings.Default.firstInstalledTranslation != true)
             {
                 btnTranslationPath.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4CAF50"));
-                btnTranslation.ToolTip = "Translation update available.";
+                btnTranslation.ToolTip = "Translation update needed.";
                 return false;
             }
 
